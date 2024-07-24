ee637da286abba6d82573ed8b6e3c92a12242f55
Remy Wang <remy.wang727@gmail.com>
2024-07-24 22:28:35 +0200

    fix: branch

--- boot.sh
+++ boot.sh
@@ -5,9 +5,9 @@ echo -e "\nBegin installation (or abort with ctrl+c)..."
 sudo apt-get update >/dev/null
 sudo apt-get install -y git >/dev/null
 
-echo "Cloning stable laptop-ubuntu..."
+echo "Cloning laptop-ubuntu..."
 rm -rf ~/.local/share/laptop-ubuntu
-git clone -b main https://github.com/remy727/laptop-ubuntu.git ~/.local/share/laptop-ubuntu >/dev/null
+git clone https://github.com/remy727/laptop-ubuntu.git ~/.local/share/laptop-ubuntu >/dev/null
 
 echo "Installation starting..."
 source ~/.local/share/laptop-ubuntu/install.sh
