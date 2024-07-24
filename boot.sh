30d57d5b5e74ef3e8f804103a4554fa8c9f71212
Remy Wang <remy.wang727@gmail.com>
2024-07-24 22:18:12 +0200

    feat: install terminal

---
+++ boot.sh
@@ -0,0 +1,13 @@
+set -e
+
+echo -e "\nBegin installation (or abort with ctrl+c)..."
+
+sudo apt-get update >/dev/null
+sudo apt-get install -y git >/dev/null
+
+echo "Cloning stable laptop-ubuntu..."
+rm -rf ~/.local/share/laptop-ubuntu
+git clone -b stable https://github.com/remy727/laptop-ubuntu.git ~/.local/share/laptop-ubuntu >/dev/null
+
+echo "Installation starting..."
+source ~/.local/share/laptop-ubuntu/install.sh
