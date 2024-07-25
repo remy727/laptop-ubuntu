672f93525852e6655cc59019a34b13fe840fee62
Remy Wang <remy.wang727@gmail.com>
2024-07-25 09:25:13 +0200

    update

--- install/terminal/001-app-ohmyzsh.sh
+++ install/terminal/001-app-ohmyzsh.sh
@@ -1,7 +1,5 @@
 echo "Installing Oh My Zsh.."
 
-sudo apt install -y zsh
-chsh -s $(which zsh)
-
 rm -rf ~/.oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+source ./oh-my-zsh.sh
