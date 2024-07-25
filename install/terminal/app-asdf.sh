fb21ff493bdb9f5e04a06d518311b2802219b576
Remy Wang <remy.wang727@gmail.com>
2024-07-25 10:36:49 +0200

    update

--- install/terminal/app-asdf.sh
+++ install/terminal/app-asdf.sh
@@ -3,3 +3,7 @@ echo "Installing asdf plugins..."
 
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
+
+asdf install nodejs 22.3.0
+asdf plugin update ruby
+asdf install ruby 3.3.4
