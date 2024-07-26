5590ade2d1ea06845fc0567796b3668fd2147a98
Remy Wang <remy.wang727@gmail.com>
2024-07-26 16:41:35 +0200

    feat: add libpq-dev

--- install/terminal/003-libraries.sh
+++ install/terminal/003-libraries.sh
@@ -3,4 +3,4 @@ echo "Installing libraries..."
 
 sudo apt-get install -y \
   autoconf patch build-essential rustc libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libgmp-dev libncurses5-dev \
-  libffi-dev libgdbm6 libgdbm-dev libdb-dev uuid-dev
+  libffi-dev libgdbm6 libgdbm-dev libdb-dev uuid-dev libpq-dev
