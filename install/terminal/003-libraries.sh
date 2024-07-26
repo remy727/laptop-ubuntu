564e2f590a02ab86a6666c72af174f4d384bd295
Remy Wang <remy.wang727@gmail.com>
2024-07-26 18:01:34 +0200

    feat: add libtool

--- install/terminal/003-libraries.sh
+++ install/terminal/003-libraries.sh
@@ -3,4 +3,5 @@ echo "Installing libraries..."
 
 sudo apt-get install -y \
   autoconf patch build-essential rustc libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libgmp-dev libncurses5-dev \
+  libtool \
   libffi-dev libgdbm6 libgdbm-dev libdb-dev uuid-dev libpq-dev
