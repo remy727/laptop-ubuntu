c2d2b9ab11818bdf3ad3b4bf380cde47e207fc48
Remy Wang <remy.wang727@gmail.com>
2024-09-14 17:41:46 -0400

    fix: update libraries

--- install/terminal/003-libraries.sh
+++ install/terminal/003-libraries.sh
@@ -4,4 +4,5 @@ echo "Installing libraries..."
 sudo apt-get install -y \
   autoconf patch build-essential rustc libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libgmp-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev uuid-dev \
   libtool \
-  libpq-dev
+  libpq-dev \
+  # libsecp256k1-dev
