de3784646a9a588d67c2c661cef37f0a8cbd6e64
Remy Wang <remy.wang727@gmail.com>
2024-09-22 16:17:48 -0400

    feat: add fuse

--- install/terminal/003-libraries.sh
+++ install/terminal/003-libraries.sh
@@ -5,4 +5,5 @@ sudo apt-get install -y \
   autoconf patch build-essential rustc libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libgmp-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev uuid-dev \
   libtool \
   libpq-dev \
+  fuse
   # libsecp256k1-dev
