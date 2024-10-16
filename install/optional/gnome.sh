7dacee3068474819ae408a06d789dfb19a2da06a
Remy Wang <remy.wang727@gmail.com>
2024-10-16 16:24:59 +0200

    feat: disable recent files

--- install/optional/gnome.sh
+++ install/optional/gnome.sh
@@ -3,3 +3,6 @@ echo "Reinstalling GNOME..."
 
 sudo apt-get update
 sudo apt-get install --reinstall gnome-control-center
+
+# Disable recent files
+gsettings set org.gnome.desktop.privacy remember-recent-files false
