35cc599a181131ff4970079ac9485ee5c3a0a228
Remy Wang <remy.wang727@gmail.com>
2024-12-21 10:56:30 -0500

    fix: redis cli

--- install/terminal/app-redis.sh
+++ install/terminal/app-redis.sh
@@ -2,7 +2,8 @@ echo
 echo "Installing Redis..."
 
 curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
+sudo chmod 644 /usr/share/keyrings/redis-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
 sudo apt-get update
-sudo apt-get install redis
+sudo apt-get install -y redis
 sudo systemctl enable redis-server
