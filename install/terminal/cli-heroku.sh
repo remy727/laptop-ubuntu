7033ba78d16b78cdee676ac977b665172f150574
Remy Wang <remy.wang727@gmail.com>
2024-12-23 03:57:58 -0500

    fix: install Heroku CLI

--- install/terminal/cli-heroku.sh
+++ install/terminal/cli-heroku.sh
@@ -1,12 +1,10 @@
 echo
 echo "Installing Heroku CLI client ..."
 
-curl https://cli-assets.heroku.com/install.sh | sh
+curl https://cli-assets.heroku.com/install-ubuntu.sh | sh
 
 echo
 echo "Installing Heroku plugins ..."
 
-heroku plugins:install heroku-accounts
-heroku plugins:install heroku-builds
+heroku plugins:install @heroku-cli/heroku-accounts
 heroku plugins:install heroku-config
-heroku plugins:install data-maintenance
