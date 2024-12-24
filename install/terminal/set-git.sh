64386a7825d30d84c48250a1f7d8b60aad6b9dc3
Remy Wang <remy.wang727@gmail.com>
2024-12-24 22:31:52 +0100

    Update set-git.sh

--- install/terminal/set-git.sh
+++ install/terminal/set-git.sh
@@ -2,3 +2,4 @@ ssh-keygen -t ed25519 -C "remy.wang727@gmail.com" -f ~/.ssh/id_ed25519 -N ""
 
 git config --global user.name "Remy Wang"
 git config --global user.email "remy.wang727@gmail.com"
+git config --global pull.rebase true
