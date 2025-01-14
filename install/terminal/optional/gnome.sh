359fef8d3254eb3353627733f4591e07f83bda69
Remy Wang <remy.wang727@gmail.com>
2025-01-14 08:40:38 +0100

    fix: gnome

--- install/terminal/optional/gnome.sh
+++ install/terminal/optional/gnome.sh
@@ -6,3 +6,4 @@ sudo apt-get install --reinstall gnome-control-center
 
 # Disable recent files
 gsettings set org.gnome.desktop.privacy remember-recent-files false
+gsettings set org.gtk.gtk4.Settings.FileChooser show-hidden true
