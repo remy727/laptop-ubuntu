1d1d10388885bccddf299c64b7076d42852b2b84
Remy Wang <remy.wang727@gmail.com>
2025-01-27 20:29:54 +0100

    fix: set git

--- install/terminal/set-git.sh
+++ install/terminal/set-git.sh
@@ -3,3 +3,4 @@ ssh-keygen -t ed25519 -C "remy.wang727@gmail.com" -f ~/.ssh/id_ed25519 -N ""
 git config --global user.name "Remy Wang"
 git config --global user.email "remy.wang727@gmail.com"
 git config --global pull.rebase true
+git config --global init.defaultBranch main
