c58a49d30c164c89f22a779e60896543e4dc1f95
Remy Wang <remy.wang727@gmail.com>
2025-01-28 15:45:01 -0500

    fix: install gnome extension manager

--- install/terminal/optional/gnome.sh
+++ install/terminal/optional/gnome.sh
@@ -3,6 +3,7 @@ echo "Reinstalling GNOME..."
 
 sudo apt-get update
 sudo apt-get install --reinstall gnome-control-center
+sudo apt-get install -y gnome-shell-extension-manager
 
 # Disable recent files
 gsettings set org.gnome.desktop.privacy remember-recent-files false
