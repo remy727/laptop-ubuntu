7df9aa9eac0e2e25dec11852aea8ac0d06ecba5e
Remy Wang <remy.wang727@gmail.com>
2025-02-17 22:47:45 +0100

    install sublime merge

--- install/desktop/app-sublime-text.sh
+++ install/desktop/app-sublime-text.sh
@@ -3,3 +3,10 @@ curl -fSsL https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | 
 echo 'deb [signed-by=/usr/share/keyrings/sublimehq-pub.gpg] https://download.sublimetext.com/ apt/stable/' | sudo tee -a /etc/apt/sources.list.d/sublime-text.list
 sudo apt update
 sudo apt install sublime-text
+
+# Install Sublime Merge
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
+sudo apt-get install apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install sublime-merge
