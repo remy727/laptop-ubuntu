2095799b9e55a25672c6f7b719bde8533221b2d2
Remy Wang <remy.wang727@gmail.com>
2025-02-20 09:51:04 +0100

    Update 003-libraries.sh

--- install/terminal/003-libraries.sh
+++ install/terminal/003-libraries.sh
@@ -1,7 +1,8 @@
 echo
 echo "Installing libraries..."
 
-sudo apt-get install -y \
+sudo apt update
+sudo apt install -y \
   autoconf patch build-essential rustc libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libgmp-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev uuid-dev \
   libtool \
   libpq-dev \
